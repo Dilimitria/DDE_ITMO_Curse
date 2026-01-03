392d4e44bc1105cdfc337c0c99604edc53def2c2
Dilimitria <dmalyga31@gmail.com>
2026-01-04 01:16:40 +0300

    Update config.py

--- etl/config.py
+++ etl/config.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
 
-# Определяем базовую директорию проекта (корень)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 RAW_DATA_PATH = BASE_DIR / "data" / "online_retail.csv"
@@ -11,4 +10,5 @@ OUTPUT_DIR = BASE_DIR / "eda" / "results"
 
 CHAMPION_THRESHOLD = 2000
 LOWSPENDER_THRESHOLD = 500
+
 AT_RISK_DAYS = 180
